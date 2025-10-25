dd5740aabcf8ff3ff874fa9befeaa01c46ec603e
incrisz <incrisz4luv@gmail.com>
2025-10-25 13:58:54 +0100

    cors allows all urls

--- config/cors.php
+++ config/cors.php
@@ -1,26 +1,5 @@
 <?php
 
-use Illuminate\Support\Arr;
-
-$defaultOrigins = array_values(array_filter([
-    'http://127.0.0.1:5500',
-    'http://localhost:5500',
-    'http://127.0.0.1:3000',
-    'http://localhost:3000',
-    'http://127.0.0.1:8000',
-    'http://localhost:8000',
-    rtrim(env('APP_URL', ''), '/'),
-    'https://lynxglobal.com.ng',
-    'https://api.lynxglobal.com.ng',
-]));
-
-$configuredOrigins = array_filter(array_map(
-    'trim',
-    explode(',', (string) env('CORS_ALLOWED_ORIGINS', ''))
-));
-
-$allowedOrigins = Arr::wrap(count($configuredOrigins) ? $configuredOrigins : $defaultOrigins);
-
 return [
 
     'paths' => [
@@ -33,11 +12,9 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => $allowedOrigins,
+    'allowed_origins' => ['*'],
 
-    'allowed_origins_patterns' => array_filter([
-        '~^https?://([a-z0-9-]+\.)?lynxglobal\.com\.ng$~i',
-    ]),
+    'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
 
