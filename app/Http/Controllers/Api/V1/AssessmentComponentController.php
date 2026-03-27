04b7409e48e240eef57c2747a4d56ac773f2ff11
incrisz <incrisz4luv@gmail.com>
2026-03-27 07:13:37 +0100

    Class "App\Http\Controllers\Api\V1\DB" not found

--- app/Http/Controllers/Api/V1/AssessmentComponentController.php
+++ app/Http/Controllers/Api/V1/AssessmentComponentController.php
@@ -7,6 +7,7 @@ use App\Models\AssessmentComponent;
 use App\Models\Result;
 use App\Models\Subject;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Str;
 use Illuminate\Validation\Rule;
